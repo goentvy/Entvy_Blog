860bb4fc6ab1f5186bf1b02fd57acd5233e6cf50
Entvy <entvyv@gmail.com>
2025-07-23 23:34:39 +0900

    file add

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
+import Subscribe from './components/Subscribe';
+import './App.css'
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Subscribe />
+      <Footer />
+    </>
+  )
+}
+
+export default App
